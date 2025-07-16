9c13615cd151d1409cefb7eb9fd23b2969a681ec
simoz <s.zini@mastervoice.it>
2025-07-16 23:53:08 +0200

    temporary allowed internal address

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,14 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
+    host: true, // Questo permette al server di essere accessibile da altri dispositivi sulla stessa rete, utile per test su mobile.
+    allowedHosts: [
+      'vapter.szini.it',
+      // Puoi aggiungere altri host qui se ne hai bisogno, ad esempio:
+      // 'localhost',
+      // '192.168.1.xxx', // Il tuo indirizzo IP locale se lo usi per test
+    ],
+    //host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/api': {
