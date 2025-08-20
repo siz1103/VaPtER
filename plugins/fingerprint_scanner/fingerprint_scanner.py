d1a11852844416a60faba8591ebd469f7f12485b
simoz <s.zini@mastervoice.it>
2025-08-20 08:17:31 +0200

    fix fingerprint rabbit error

--- plugins/fingerprint_scanner/fingerprint_scanner.py
+++ plugins/fingerprint_scanner/fingerprint_scanner.py
@@ -102,8 +102,22 @@ class FingerprintScanner:
                 self.channel = self.connection.channel()
                 
                 # Declare queues
-                self.channel.queue_declare(queue=settings.FINGERPRINT_SCAN_REQUEST_QUEUE, durable=True)
-                self.channel.queue_declare(queue=settings.SCAN_STATUS_UPDATE_QUEUE, durable=True)
+                self.channel.queue_declare(
+                    queue=settings.FINGERPRINT_SCAN_REQUEST_QUEUE, 
+                    durable=True,
+                    arguments={
+                        'x-message-ttl': 3600000,  # 1 hour TTL
+                        'x-max-length': 10000
+                    }
+                )
+                self.channel.queue_declare(
+                    queue=settings.SCAN_STATUS_UPDATE_QUEUE, 
+                    durable=True,
+                    arguments={
+                        'x-message-ttl': 3600000,  # 1 hour TTL
+                        'x-max-length': 10000
+                    }
+                )
                 
                 logger.info("Successfully connected to RabbitMQ")
                 return True
